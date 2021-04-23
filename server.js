fdec0dfa88fa8d2076bd6f137c384b20784b1a34
makhatadze <vitomaxatadze@gmail.com>
2021-04-23 16:26:33 +0400

    Listen to socket.

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const socket = require('socket.io');
 const io = socket(server);
 
 
+io.on('connection',socket => {
+    socket.emit('your id', socket.id);
+    socket.on('send message',body => {
+        io.emit('message',body)
+    })
+})
+
+
 server.listen(
     8000,
     () => console.log('Server is running on port 8000')

9d300ffeb0d1499446b8eb7e03a389bba8b2bf90
makhatadze <vitomaxatadze@gmail.com>
2021-04-23 16:35:17 +0400

    Create Page styled component.

---
+++ web/src/App.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const Page = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  align-items: center;
+  background-color: #46516e;
+  flex-direction: column;
+`;
